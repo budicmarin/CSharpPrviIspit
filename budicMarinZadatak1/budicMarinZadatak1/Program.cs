64d2c21cdc7e8ce6f225e8ca44f7c74584d34e1c
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-02-06 10:11:51 +0100

    Add files via upload

---
+++ budicMarinZadatak1/budicMarinZadatak1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int zbroj=0;
+            for (int i=0;i<3;i++)
+            {
+                Console.WriteLine("Upisi broj");
+                a = Convert.ToInt32(Console.ReadLine());
+                zbroj += a;
+            }
+            Console.WriteLine("Prosjek brojeva je"+(float)zbroj/(float)3);
+            Console.ReadKey();
+        }
+       
+    }
+}
