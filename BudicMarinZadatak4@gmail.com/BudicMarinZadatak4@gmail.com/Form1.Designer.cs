a05ef371670e3bbb88d292e0f54b4beb0220a6e8
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-02-06 18:39:13 +0100

    Add files via upload

---
+++ BudicMarinZadatak4@gmail.com/BudicMarinZadatak4@gmail.com/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BudicMarinZadatak4_gmail.com
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPrviBroj = new System.Windows.Forms.Label();
+            this.labelDrugiBroj = new System.Windows.Forms.Label();
+            this.buttonZbroj = new System.Windows.Forms.Button();
+            this.textBoxPrviBroj = new System.Windows.Forms.TextBox();
+            this.textBoxDrugiBroj = new System.Windows.Forms.TextBox();
+            this.buttonRazlika = new System.Windows.Forms.Button();
+            this.buttonKoeficijent = new System.Windows.Forms.Button();
+            this.buttonUmnožak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelPrviBroj
+            // 
+            this.labelPrviBroj.AutoSize = true;
+            this.labelPrviBroj.Location = new System.Drawing.Point(13, 13);
+            this.labelPrviBroj.Name = "labelPrviBroj";
+            this.labelPrviBroj.Size = new System.Drawing.Size(25, 17);
+            this.labelPrviBroj.TabIndex = 0;
+            this.labelPrviBroj.Text = "A=";
+            // 
+            // labelDrugiBroj
+            // 
+            this.labelDrugiBroj.AutoSize = true;
+            this.labelDrugiBroj.Location = new System.Drawing.Point(13, 34);
+            this.labelDrugiBroj.Name = "labelDrugiBroj";
+            this.labelDrugiBroj.Size = new System.Drawing.Size(25, 17);
+            this.labelDrugiBroj.TabIndex = 1;
+            this.labelDrugiBroj.Text = "B=";
+            // 
+            // buttonZbroj
+            // 
+            this.buttonZbroj.Location = new System.Drawing.Point(181, 5);
+            this.buttonZbroj.Name = "buttonZbroj";
+            this.buttonZbroj.Size = new System.Drawing.Size(75, 23);
+            this.buttonZbroj.TabIndex = 2;
+            this.buttonZbroj.Text = "Zbroj";
+            this.buttonZbroj.UseVisualStyleBackColor = true;
+            this.buttonZbroj.Click += new System.EventHandler(this.ButtonZbroj_Click);
+            // 
+            // textBoxPrviBroj
+            // 
+            this.textBoxPrviBroj.Location = new System.Drawing.Point(45, 6);
+            this.textBoxPrviBroj.Name = "textBoxPrviBroj";
+            this.textBoxPrviBroj.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPrviBroj.TabIndex = 3;
+            this.textBoxPrviBroj.Text = "Unesite broj";
+            this.textBoxPrviBroj.TextChanged += new System.EventHandler(this.TextBoxPrviBroj_TextChanged);
+            // 
+            // textBoxDrugiBroj
+            // 
+            this.textBoxDrugiBroj.Location = new System.Drawing.Point(45, 35);
+            this.textBoxDrugiBroj.Name = "textBoxDrugiBroj";
+            this.textBoxDrugiBroj.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDrugiBroj.TabIndex = 4;
+            this.textBoxDrugiBroj.Text = "Drugi broj";
+            this.textBoxDrugiBroj.TextChanged += new System.EventHandler(this.TextBoxDrugiBroj_TextChanged);
+            // 
+            // buttonRazlika
+            // 
+            this.buttonRazlika.Location = new System.Drawing.Point(181, 35);
+            this.buttonRazlika.Name = "buttonRazlika";
+            this.buttonRazlika.Size = new System.Drawing.Size(75, 23);
+            this.buttonRazlika.TabIndex = 5;
+            this.buttonRazlika.Text = "Razlika";
+            this.buttonRazlika.UseVisualStyleBackColor = true;
+            this.buttonRazlika.Click += new System.EventHandler(this.ButtonRazlika_Click);
+            // 
+            // buttonKoeficijent
+            // 
+            this.buttonKoeficijent.Location = new System.Drawing.Point(262, 35);
+            this.buttonKoeficijent.Name = "buttonKoeficijent";
+            this.buttonKoeficijent.Size = new System.Drawing.Size(87, 23);
+            this.buttonKoeficijent.TabIndex = 7;
+            this.buttonKoeficijent.Text = "Koeficijent";
+            this.buttonKoeficijent.UseVisualStyleBackColor = true;
+            this.buttonKoeficijent.Click += new System.EventHandler(this.ButtonKoeficijent_Click);
+            // 
+            // buttonUmnožak
+            // 
+            this.buttonUmnožak.Location = new System.Drawing.Point(262, 5);
+            this.buttonUmnožak.Name = "buttonUmnožak";
+            this.buttonUmnožak.Size = new System.Drawing.Size(87, 23);
+            this.buttonUmnožak.TabIndex = 6;
+            this.buttonUmnožak.Text = "Umnožak";
+            this.buttonUmnožak.UseVisualStyleBackColor = true;
+            this.buttonUmnožak.Click += new System.EventHandler(this.ButtonUmnožak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 86);
+            this.Controls.Add(this.buttonKoeficijent);
+            this.Controls.Add(this.buttonUmnožak);
+            this.Controls.Add(this.buttonRazlika);
+            this.Controls.Add(this.textBoxDrugiBroj);
+            this.Controls.Add(this.textBoxPrviBroj);
+            this.Controls.Add(this.buttonZbroj);
+            this.Controls.Add(this.labelDrugiBroj);
+            this.Controls.Add(this.labelPrviBroj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPrviBroj;
+        private System.Windows.Forms.Label labelDrugiBroj;
+        private System.Windows.Forms.Button buttonZbroj;
+        private System.Windows.Forms.TextBox textBoxPrviBroj;
+        private System.Windows.Forms.TextBox textBoxDrugiBroj;
+        private System.Windows.Forms.Button buttonRazlika;
+        private System.Windows.Forms.Button buttonKoeficijent;
+        private System.Windows.Forms.Button buttonUmnožak;
+    }
+}
+
---
+++ BudicMarinZadatak4@gmail.com/BudicMarinZadatak4@gmail.com/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BudicMarinZadatak4_gmail.com
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonZbroj_Click(object sender, EventArgs e)
+        {
+            int value1 = 0;
+            int value2 = 0;
+            
+            
+
+            if (int.TryParse(textBoxPrviBroj.Text, out value1))
+            {
+
+                if (int.TryParse(textBoxDrugiBroj.Text, out value2))
+                {
+                    string rezultat = value1 + "+" + value2 + "=" + (value1 + value2);
+                    DialogResult result = MessageBox.Show(rezultat," Upotreba Windows formi",MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxPrviBroj.Text = " ";
+                        textBoxDrugiBroj.Text = " ";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nisi upisao broj"); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nisi upisao broj");
+            }
+        }
+
+        private void ButtonUmnožak_Click(object sender, EventArgs e)
+        {
+            int value1 = 0;
+            int value2 = 0;
+
+
+
+            if (int.TryParse(textBoxPrviBroj.Text, out value1))
+            {
+
+                if (int.TryParse(textBoxDrugiBroj.Text, out value2))
+                {
+                    string rezultat = value1 + "*" + value2 + "=" + (value1 *value2);
+                    DialogResult result = MessageBox.Show(rezultat, " Upotreba Windows formi", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxPrviBroj.Text = " ";
+                        textBoxDrugiBroj.Text = " ";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nisi upisao broj");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nisi upisao broj");
+            }
+        }
+
+        private void ButtonRazlika_Click(object sender, EventArgs e)
+        {
+            int value1 = 0;
+            int value2 = 0;
+
+
+
+            if (int.TryParse(textBoxPrviBroj.Text, out value1))
+            {
+
+                if (int.TryParse(textBoxDrugiBroj.Text, out value2))
+                {
+                    string rezultat = value1 + "-" + value2 + "=" + (value1 - value2);
+                    DialogResult result = MessageBox.Show(rezultat, " Upotreba Windows formi", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxPrviBroj.Text = " ";
+                        textBoxDrugiBroj.Text = " ";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nisi upisao broj");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nisi upisao broj");
+            }
+        }
+
+        private void ButtonKoeficijent_Click(object sender, EventArgs e)
+        {
+            int value1 = 0;
+            int value2 = 0;
+
+
+
+            if (int.TryParse(textBoxPrviBroj.Text, out value1))
+            {
+
+                if (int.TryParse(textBoxDrugiBroj.Text, out value2))
+                {
+                    string rezultat = value1 + "/" + value2 + "=" + ((double)value1 /(double) value2);
+                    DialogResult result = MessageBox.Show(rezultat, " Upotreba Windows formi", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        textBoxPrviBroj.Text = " ";
+                        textBoxDrugiBroj.Text = " ";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nisi upisao broj");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nisi upisao broj");
+            }
+        }
+
+        private void TextBoxPrviBroj_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxDrugiBroj_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
